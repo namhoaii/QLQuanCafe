afaef149c985b035ef45e942878a25bf0a710f75
Hoai Nam <hoainamsmurf@gmail.com>
2023-11-16 08:25:53 +0700

    create mvvm

--- QLQuanCafe/QLQuanCafe.Android/Resources/Resource.designer.cs
+++ QLQuanCafe/QLQuanCafe.Android/Resources/Resource.designer.cs
@@ -16726,6 +16726,9 @@ namespace QLQuanCafe.Droid
 			// aapt resource value: 0x7F070091
 			public const int tooltip_frame_light = 2131165329;
 			
+			// aapt resource value: 0x7F070092
+			public const int user_4_line = 2131165330;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
