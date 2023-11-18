561d830447f2540ecf1cbeeb4c6aeac0d5daf2df
Hoai Nam <hoainamsmurf@gmail.com>
2023-11-18 10:12:26 +0700

    create login page signup page forgotPassword page

---
+++ QLQuanCafe/QLQuanCafe.Android/CustomEntryRenderer.cs
@@ -0,0 +1,88 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Text.Method;
+using Android.Views;
+using Android.Widget;
+using Java.Interop;
+using QLQuanCafe.Customers;
+using QLQuanCafe.Droid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace QLQuanCafe.Droid
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context) { }
+
+        /*protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null && e.NewElement != null)
+            {
+                // Set background to transparent
+                GradientDrawable gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetColor(Android.Graphics.Color.Transparent);
+                Control.SetBackground(gradientDrawable);
+
+                // Remove suggestions
+                Control.InputType = InputTypes.TextFlagNoSuggestions;
+
+                // Set hint color (you can set a specific color here instead of Transparent)
+                Control.SetHintTextColor(Android.Graphics.Color.AliceBlue); // Set a color that contrasts with your background
+            }
+        }*/
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == nameof(Entry.IsPassword))
+            {
+                UpdateIsPassword();
+            }
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null && e.NewElement != null)
+            {
+                // Set background to transparent
+                GradientDrawable gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetColor(Android.Graphics.Color.Transparent);
+                Control.SetBackground(gradientDrawable);
+
+                // Remove suggestions
+                Control.InputType = InputTypes.TextFlagNoSuggestions;
+
+                // Set hint color
+                Control.SetHintTextColor(Android.Graphics.Color.AliceBlue);
+
+                // Update IsPassword when Element changes
+                UpdateIsPassword();
+            }
+        }
+
+        private void UpdateIsPassword()
+        {
+            if (Control != null && Element is Entry entry)
+            {
+                Control.TransformationMethod = entry.IsPassword ? PasswordTransformationMethod.Instance : null;
+            }
+        }
+
+
+    }
+}
