f5d5c80a6a658364cb4711755cd9c8dbe984a25a
Hoai Nam <hoainamsmurf@gmail.com>
2023-11-29 20:35:13 +0700

    update v1.0.1

--- QLQuanCafe/QLQuanCafe/ViewModels/SignupViewModel.cs
+++ QLQuanCafe/QLQuanCafe/ViewModels/SignupViewModel.cs
@@ -105,6 +105,14 @@ namespace QLQuanCafe.ViewModels
 
             #region Rule
 
+            //Kiểm định dạng User
+            string patternUser = @"^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$";
+            if (!Regex.IsMatch(_username, patternUser))
+            {
+                isVal = false;
+                UsernameError = "Tên đăng nhập ít nhất 1 chữ, 1 số và chỉ chấp nhận các ký tự chữ và số";
+            }
+
             //Kiểm tra định dạng email
             string patternEmail = @"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$";
             if (!Regex.IsMatch(_email, patternEmail))
